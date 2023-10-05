13e6e21e274044f034a45c0e36dcd9e5b3b8dbb4
firzandiads <firzandi200803@gmail.com>
2023-10-05 15:30:44 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return view('users.index', ['users' => $users]);
+    }
+
+    public function store()
+    {
+        return view('users.tambah');
+    }
+
+    public function lihat()
+    {
+        return view('users.lihat');
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YiYi - Home</title>
+    <style>
+        /* Reset CSS */
+        body, ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Navbar styling */
+        nav {
+            background-color: #333;
+            color: #fff;
+            display: flex;
+            align-items: center;
+            padding: 10px;
+        }
+
+        .logo {
+            font-size: 24px;
+            font-weight: bold;
+            margin-right: auto;
+        }
+
+        ul.nav-menu {
+            list-style: none;
+            display: flex;
+        }
+
+        ul.nav-menu li {
+            margin-right: 20px;
+        }
+
+        ul.nav-menu li:last-child {
+            margin-right: 0;
+        }
+
+        ul.nav-menu li a {
+            text-decoration: none;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        /* Page content styling */
+        section {
+            padding: 20px;
+        }
+
+        h1 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+
+        p {
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+    <!-- Navbar -->
+    <nav>
+        <div class="logo">YiYi</div>
+        <ul class="nav-menu">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/services">Services</a></li>
+            <!-- Tambahkan lebih banyak item menu jika diperlukan -->
+        </ul>
+    </nav>
+
+    <!-- Konten Halaman Home -->
+    <section>
+        <h1>Home</h1>
+        <p>Selamat datang di halaman utama YiYi.</p>
+    </section>
+
+    <!-- Tambahkan konten tambahan di bawah ini -->
+
+    <!-- Tambahkan script JavaScript Anda di sini jika diperlukan -->
+
+</body>
+</html>
